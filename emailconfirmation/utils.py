0ac8ca9e7894905e0317d2ab6e0c5fb70c7115bf
Hakan Bakkalbasi <hbakkalbasi@gmail.com>
2012-08-11 22:47:45 -0400

    Forgot to also change import statement ...

--- emailconfirmation/utils.py
+++ emailconfirmation/utils.py
@@ -7,7 +7,7 @@ def get_send_mail():
     """
     # favour django-mailer but fall back to django.core.mail
     if "django_mailer" in settings.INSTALLED_APPS:
-        from mailer import send_mail
+        from django_mailer import send_mail
     else:
         from django.core.mail import send_mail as _send_mail
         def send_mail(*args, **kwargs):
